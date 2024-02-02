1d3c1a8e48a1108716490ce8228deb7530149efc
wook099 <89290934+wook099@users.noreply.github.com>
2024-02-02 20:26:54 +0900

    Add files via upload

---
+++ D2/Main1.java
@@ -0,0 +1,12 @@
+package D2;
+
+public class Main1 {
+
+    public static final int SCREEN_WIDTH=1280;
+    public static final int SCREEN_HEIGHT=720;
+
+    public static void main(String[] args){
+       new DynamicBeat();
+    }
+
+}
